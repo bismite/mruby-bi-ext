33a9d51d186d152eee97ab34df4b26d47ee7f2c8
kbys <work4kbys@gmail.com>
2018-10-29 15:23:16 +0900

    add action callback and remove action method

--- mrblib/bi_ext.rb
+++ mrblib/bi_ext.rb
@@ -1,16 +1,48 @@
 
 class Bi
-  def self.add_action(node,action)
-    @@bi.add_action node, action
+  def self.add_action(node,action,callback)
+    @@bi.add_action node, action, callback
+  end
+  def self.remove_action(action)
+    @@bi.remove_action action
   end
 end
 
+class Bi::Action
+  attr_accessor :callback
+end
+
 class Bi::Node
-  def run_action(action)
-    Bi::add_action self,action
+  def run_action(action,autoremove=true,callback_symbol=nil,&callback)
+    if callback_symbol
+      Bi::add_action self,action,callback_symbol
+    elsif callback
+      Bi::add_action self,action,callback
+    elsif autoremove and not action.is_a? Bi::Action::Repeat
+      Bi::add_action self,action,lambda{|node,action| node.remove_action action }
+    else
+      Bi::add_action self,action,nil
+    end
     @actions ||= []
     @actions << action
     action.start self
+  end
+  def remove_action(action)
+    if @actions and @actions.include? action
+      Bi::remove_action action
+      @actions.delete action
+    end
+  end
+  def actions
+    @actions
+  end
+  def remove_all_actions
+    if @actions
+      @actions.each{|a|
+        Bi::remove_action a
+      }
+      @actions.clear
+    end
   end
 end
 
--- src/action.c
+++ src/action.c
@@ -8,18 +8,67 @@
 #include <stdlib.h>
 #include "bi_ext_inner_macro.h"
 
+typedef struct {
+  mrb_state *mrb;
+  mrb_value node;
+  mrb_value action;
+} mrb_action_finish_callback_context;
+
+void mrb_action_finish_callback(BiAction* action, void* context)
+{
+  mrb_action_finish_callback_context *c = context;
+  mrb_state *mrb = c->mrb;
+  mrb_value self = c->action;
+  mrb_value callback = mrb_iv_get(mrb, self, mrb_intern_cstr(mrb,"@callback") );
+  mrb_value argv[2] = { c->node, self };
+  if( mrb_symbol_p(callback) ){
+    mrb_funcall_argv(mrb,c->node,mrb_symbol(callback),2,argv);
+  }else if( mrb_type(callback) == MRB_TT_PROC ) {
+    mrb_yield_argv(mrb,callback,2,argv);
+  }
+}
+
+void mrb_action_free(mrb_state *mrb,void* p){
+  BiAction *action = p;
+  free(action->on_finish_callback_context);
+  mrb_free(mrb,action);
+}
+
 // Bi::add_action
 static mrb_value mrb_add_action(mrb_state *mrb, mrb_value self)
 {
-    mrb_value node_obj, action_obj;
-    mrb_get_args(mrb, "oo", &node_obj, &action_obj);
+    mrb_value node_obj, action_obj, callback_obj;
+    mrb_get_args(mrb, "ooo", &node_obj, &action_obj, &callback_obj);
 
     // TODO: check error
-    BiContext *c = DATA_PTR(self);
+    BiContext *context = DATA_PTR(self);
     BiNode *node = DATA_PTR(node_obj);
     BiAction *action = DATA_PTR(action_obj);
 
-    bi_add_action(c,node,action);
+    mrb_iv_set(mrb, action_obj, mrb_intern_cstr(mrb,"@callback"), callback_obj );
+
+    mrb_action_finish_callback_context* c = malloc(sizeof(mrb_action_finish_callback_context));
+    c->mrb = mrb;
+    c->node = node_obj;
+    c->action = action_obj;
+    action->on_finish_callback_context = c;
+    action->on_finish = mrb_action_finish_callback;
+
+    bi_add_action(context,node,action);
+
+    return self;
+}
+
+static mrb_value mrb_remove_action(mrb_state *mrb, mrb_value self)
+{
+    mrb_value action_obj;
+    mrb_get_args(mrb, "o", &action_obj);
+
+    // TODO: check error
+    BiContext *c = DATA_PTR(self);
+    BiAction *action = DATA_PTR(action_obj);
+
+    bi_remove_action(c,action);
 
     return self;
 }
@@ -27,18 +76,13 @@ static mrb_value mrb_add_action(mrb_state *mrb, mrb_value self)
 //
 // Bi::Action class
 //
-static struct mrb_data_type const mrb_action_data_type = { "Action", mrb_free };
+static struct mrb_data_type const mrb_action_data_type = { "Action", mrb_action_free };
 
 static mrb_value mrb_action_initialize(mrb_state *mrb, mrb_value self)
 {
-    BiAction *action = DATA_PTR(self);
-    if (action == NULL) {
-      action = mrb_malloc(mrb, sizeof(BiAction));
-      if (NULL == action) {
-        mrb_raise(mrb, E_RUNTIME_ERROR, "insufficient memory.");
-      }
-    }
-
+    BiAction *action;
+    action = mrb_malloc(mrb, sizeof(BiAction));
+    action->on_finish_callback_context = NULL;
     DATA_PTR(self) = action;
     DATA_TYPE(self) = &mrb_action_data_type;
 
@@ -125,6 +169,7 @@ void mrb_init_action(mrb_state *mrb, struct RClass *bi)
   MRB_SET_INSTANCE_TT(action, MRB_TT_DATA);
 
   mrb_define_method(mrb, bi, "add_action", mrb_add_action, MRB_ARGS_REQ(2)); // node,action
+  mrb_define_method(mrb, bi, "remove_action", mrb_remove_action, MRB_ARGS_REQ(1)); // action
 
   mrb_define_method(mrb, action, "initialize", mrb_action_initialize, MRB_ARGS_NONE());
   mrb_define_method(mrb, action, "start", mrb_action_start, MRB_ARGS_REQ(1)); // node
