b5c4b8399999686ff7ac08229aa86639ff98319f
kbys <work4kbys@gmail.com>
2018-11-05 04:55:03 +0900

    new action system

--- mrblib/bi_ext.rb
+++ mrblib/bi_ext.rb
@@ -1,68 +1,28 @@
 
-class Bi
-  def self.add_action(node,action,callback)
-    @@bi.add_action node, action, callback
-  end
-  def self.remove_action(action)
-    @@bi.remove_action action
-  end
-end
-
-class Bi::Action
-  attr_accessor :callback
-end
-
 class Bi::Node
-  def run_action(action,autoremove=true,callback_symbol=nil,&callback)
-    if callback_symbol
-      Bi::add_action self,action,callback_symbol
-    elsif callback
-      Bi::add_action self,action,callback
-    elsif autoremove and not action.is_a? Bi::Action::Repeat
-      Bi::add_action self,action,lambda{|node,action| node.remove_action action }
-    else
-      Bi::add_action self,action,nil
-    end
+  attr_reader :actions
+  def add_action(action)
     @actions ||= []
     @actions << action
-    action.start self
+    self._add_action action
+    action
   end
   def remove_action(action)
-    if @actions and @actions.include? action
-      Bi::remove_action action
-      @actions.delete action
-    end
-  end
-  def actions
-    @actions
+    @actions ||= []
+    @actions.delete action
+    self._remove_action action
+    action
   end
   def remove_all_actions
     if @actions
       @actions.each{|a|
-        Bi::remove_action a
+        self._remove_action a
       }
       @actions.clear
     end
   end
 end
 
-class Bi::Action::MoveTo < Bi::Action
-  def initialize(duration,x,y)
-    super
-    self.move_to duration,x,y
-  end
-end
-
-class Bi::Action::Sequence < Bi::Action
-  def initialize(actions)
-    super
-    self.sequence actions
-  end
-end
-
-class Bi::Action::Repeat < Bi::Action
-  def initialize(action)
-    super
-    self.repeat action
-  end
+class Bi::Action::Base
+  attr_accessor :callback
 end
--- src/action.c
+++ src/action.c
@@ -8,9 +8,10 @@
 #include <stdlib.h>
 #include "bi_ext_inner_macro.h"
 
+// callback on finish
+
 typedef struct {
   mrb_state *mrb;
-  mrb_value node;
   mrb_value action;
 } mrb_action_finish_callback_context;
 
@@ -19,43 +20,34 @@ void mrb_action_finish_callback(BiAction* action, void* context)
   mrb_action_finish_callback_context *c = context;
   mrb_state *mrb = c->mrb;
   mrb_value self = c->action;
+  mrb_value node = mrb_obj_value(action->node->userdata);
   mrb_value callback = mrb_iv_get(mrb, self, mrb_intern_cstr(mrb,"@callback") );
-  mrb_value argv[2] = { c->node, self };
+  mrb_value argv[2] = { node, self };
   if( mrb_symbol_p(callback) ){
-    mrb_funcall_argv(mrb,c->node,mrb_symbol(callback),2,argv);
+    mrb_funcall_argv(mrb,node,mrb_symbol(callback),2,argv);
   }else if( mrb_type(callback) == MRB_TT_PROC ) {
     mrb_yield_argv(mrb,callback,2,argv);
   }
 }
 
-void mrb_action_free(mrb_state *mrb,void* p){
-  BiAction *action = p;
-  free(action->on_finish_callback_context);
-  mrb_free(mrb,action);
-}
 
-// Bi::add_action
+//
+// Bi::Node
+//
+
 static mrb_value mrb_add_action(mrb_state *mrb, mrb_value self)
 {
-    mrb_value node_obj, action_obj, callback_obj;
-    mrb_get_args(mrb, "ooo", &node_obj, &action_obj, &callback_obj);
-
+    BiNode *node;
+    BiAction *action;
+    mrb_action_finish_callback_context *callback_context;
+    mrb_value action_obj;
+    mrb_get_args(mrb, "o", &action_obj);
     // TODO: check error
-    BiContext *context = DATA_PTR(self);
-    BiNode *node = DATA_PTR(node_obj);
-    BiAction *action = DATA_PTR(action_obj);
-
-    mrb_iv_set(mrb, action_obj, mrb_intern_cstr(mrb,"@callback"), callback_obj );
-
-    mrb_action_finish_callback_context* c = malloc(sizeof(mrb_action_finish_callback_context));
-    c->mrb = mrb;
-    c->node = node_obj;
-    c->action = action_obj;
-    action->on_finish_callback_context = c;
-    action->on_finish = mrb_action_finish_callback;
-
-    bi_add_action(context,node,action);
-
+    node = DATA_PTR(self);
+    action = DATA_PTR(action_obj);
+    callback_context = action->on_finish_callback_context;
+    bi_add_action(node,action);
+    bi_action_start(node,action,bi_get_now());
     return self;
 }
 
@@ -63,118 +55,189 @@ static mrb_value mrb_remove_action(mrb_state *mrb, mrb_value self)
 {
     mrb_value action_obj;
     mrb_get_args(mrb, "o", &action_obj);
-
     // TODO: check error
-    BiContext *c = DATA_PTR(self);
+    BiNode *node = DATA_PTR(self);
     BiAction *action = DATA_PTR(action_obj);
-
-    bi_remove_action(c,action);
-
+    bi_remove_action(node,action);
     return self;
 }
 
 //
-// Bi::Action class
+// Bi::Action
 //
+
+void mrb_action_free(mrb_state *mrb,void* p){
+  BiAction *action = p;
+  mrb_free(mrb,action->timer);
+  mrb_free(mrb,action->on_finish_callback_context);
+  mrb_free(mrb,action);
+}
+
 static struct mrb_data_type const mrb_action_data_type = { "Action", mrb_action_free };
 
-static mrb_value mrb_action_initialize(mrb_state *mrb, mrb_value self)
+static BiAction* action_initialize(mrb_state *mrb, mrb_value *self,mrb_value callback,size_t size)
 {
-    BiAction *action;
-    action = mrb_malloc(mrb, sizeof(BiAction));
-    action->on_finish_callback_context = NULL;
-    DATA_PTR(self) = action;
-    DATA_TYPE(self) = &mrb_action_data_type;
+  BiAction* action = mrb_malloc(mrb,sizeof(BiAction));
+  bi_action_init(action);
+  action->timer = mrb_malloc(mrb,sizeof(BiTimer));
+  action->action_data = mrb_malloc(mrb,size);
+  DATA_PTR(*self) = action;
+  DATA_TYPE(*self) = &mrb_action_data_type;
 
-    return self;
-}
+  mrb_action_finish_callback_context* c = mrb_malloc(mrb,sizeof(mrb_action_finish_callback_context));
+  c->mrb = mrb;
+  c->action = *self;
+  action->on_finish_callback_context = c;
+  action->on_finish = mrb_action_finish_callback;
 
-static mrb_value mrb_action_start(mrb_state *mrb, mrb_value self)
-{
-    mrb_value node_obj;
-    mrb_get_args(mrb, "o", &node_obj);
+  mrb_iv_set(mrb, *self, mrb_intern_cstr(mrb,"@callback"), callback );
 
-    // TODO: check error
-    BiAction *action = DATA_PTR(self);
-    BiNode *node = DATA_PTR(node_obj);
-
-    bi_action_start(node,action,bi_get_now());
-
-    return self;
+  return action;
 }
 
 //
-// Actions
+// Bi::Action::Base
 //
 
-static mrb_value mrb_action_move_to(mrb_state *mrb, mrb_value self)
+static mrb_value mrb_action_base_initialize(mrb_state *mrb, mrb_value self)
 {
-    mrb_float duration;
-    mrb_int x,y;
-    mrb_get_args(mrb, "fii", &duration, &x, &y);
-
-    // TODO: check error
-    BiAction *action = DATA_PTR(self);
-
-    bi_action_move_to_init(action,duration,x,y);
-
-    return self;
+  BiAction *action;
+  mrb_value callback;
+  mrb_get_args(mrb, "o", &callback);
+  action = action_initialize(mrb,&self,callback,0);
+  bi_action_base_init(action);
+  return self;
 }
 
-static mrb_value mrb_action_sequence(mrb_state *mrb, mrb_value self)
+//
+// Bi::Action::MoveTo
+//
+
+static mrb_value mrb_action_move_to_initialize(mrb_state *mrb, mrb_value self)
 {
-    mrb_value array;
-    mrb_get_args(mrb, "A", &array);
-
-    // TODO: check error
-    BiAction *action = DATA_PTR(self);
-
-    mrb_int len = RARRAY_LEN(array);
-    BiAction *actions[len];
-
-    for(int i=0;i<len;i++){
-      mrb_value a = mrb_ary_ref(mrb,array,i);
-      actions[i] = DATA_PTR(a);
-    }
-
-    bi_action_sequence_init(action, len, actions );
-
-    // retain
-    mrb_iv_set(mrb, self, mrb_intern_cstr(mrb,"@sequenced_actions"), array);
-
-    return self;
+  mrb_float duration;
+  mrb_int x,y;
+  mrb_value callback;
+  BiAction *action;
+  mrb_get_args(mrb, "fiio", &duration, &x, &y, &callback);
+  action = action_initialize(mrb,&self,callback,sizeof(BiActionMoveTo));
+  bi_action_move_to_init(action,duration,x,y);
+  return self;
 }
 
-static mrb_value mrb_action_repeat(mrb_state *mrb, mrb_value self)
+//
+// Bi::Action::RotateTo
+//
+
+static mrb_value mrb_action_rotate_to_initialize(mrb_state *mrb, mrb_value self)
 {
-    mrb_value obj;
-    mrb_get_args(mrb, "o", &obj);
-
-    // TODO: check error
-    BiAction *action = DATA_PTR(self);
-    BiAction *target = DATA_PTR(obj);
-
-    bi_action_repeat_init(action, target);
-
-    // retain
-    mrb_iv_set(mrb, self, mrb_intern_cstr(mrb,"@repeating_action"), obj);
-
-    return self;
+  mrb_float duration;
+  mrb_float angle;
+  mrb_value callback;
+  BiAction *action;
+  mrb_get_args(mrb, "ffo", &duration, &angle, &callback);
+  action = action_initialize(mrb,&self,callback,sizeof(BiActionRotateTo));
+  bi_action_rotate_to_init(action,duration,angle);
+  return self;
 }
+
+//
+// Bi::Action::RotateBy
+//
+
+static mrb_value mrb_action_rotate_by_initialize(mrb_state *mrb, mrb_value self)
+{
+  mrb_float duration;
+  mrb_float angle;
+  mrb_value callback;
+  BiAction *action;
+  mrb_get_args(mrb, "ffo", &duration, &angle, &callback);
+  action = action_initialize(mrb,&self,callback,sizeof(BiActionRotateBy));
+  bi_action_rotate_by_init(action,duration,angle);
+  return self;
+}
+
+//
+// Bi::Action::Repeat
+//
+
+static mrb_value mrb_action_repeat_initialize(mrb_state *mrb, mrb_value self)
+{
+  mrb_float duration;
+  mrb_value target_action_obj;
+  mrb_value callback;
+  BiAction *action;
+  mrb_get_args(mrb, "oo", &target_action_obj, &callback);
+  mrb_iv_set(mrb, self, mrb_intern_cstr(mrb,"@_target_action_"), target_action_obj);
+  action = action_initialize(mrb,&self,callback,sizeof(BiActionRepeat));
+  bi_action_repeat_init( action, DATA_PTR(target_action_obj) );
+  return self;
+}
+
+//
+// Bi::Action::Sequence
+//
+
+static mrb_value mrb_action_sequence_initialize(mrb_state *mrb, mrb_value self)
+{
+  mrb_value actions_obj;
+  mrb_value callback;
+  BiAction *action;
+  BiAction **actions;
+  mrb_get_args(mrb, "Ao", &actions_obj, &callback);
+  mrb_iv_set(mrb, self, mrb_intern_cstr(mrb,"@_actions_"), actions_obj);
+  action = action_initialize(mrb,&self,callback,sizeof(BiActionSequence));
+  actions = malloc( sizeof(BiAction*) * RARRAY_LEN(actions_obj) );
+  for(int i=0; i<RARRAY_LEN(actions_obj); i++ ) {
+    actions[i] = DATA_PTR( RARRAY_PTR(actions_obj)[i] );
+  }
+  bi_action_sequence_init(action, RARRAY_LEN(actions_obj), actions );
+  free(actions);
+  return self;
+}
+
+//
+//
+//
 
 void mrb_init_action(mrb_state *mrb, struct RClass *bi)
 {
+  struct RClass *node;
   struct RClass *action;
-  action = mrb_define_class_under(mrb, bi, "Action", mrb->object_class);
-  MRB_SET_INSTANCE_TT(action, MRB_TT_DATA);
+  struct RClass *base;
+  struct RClass *move_to;
+  struct RClass *rotate_to;
+  struct RClass *rotate_by;
+  struct RClass *repeat;
+  struct RClass *sequence;
 
-  mrb_define_method(mrb, bi, "add_action", mrb_add_action, MRB_ARGS_REQ(2)); // node,action
-  mrb_define_method(mrb, bi, "remove_action", mrb_remove_action, MRB_ARGS_REQ(1)); // action
+  node = mrb_class_get_under(mrb, bi, "Node");
+  mrb_define_method(mrb, node, "_add_action", mrb_add_action, MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, node, "_remove_action", mrb_remove_action, MRB_ARGS_REQ(1));
 
-  mrb_define_method(mrb, action, "initialize", mrb_action_initialize, MRB_ARGS_NONE());
-  mrb_define_method(mrb, action, "start", mrb_action_start, MRB_ARGS_REQ(1)); // node
+  action = mrb_define_module_under(mrb,bi,"Action");
 
-  mrb_define_method(mrb, action, "move_to", mrb_action_move_to, MRB_ARGS_REQ(3)); // duration,x,y
-  mrb_define_method(mrb, action, "sequence", mrb_action_sequence, MRB_ARGS_REQ(1)); // actions
-  mrb_define_method(mrb, action, "repeat", mrb_action_repeat, MRB_ARGS_REQ(1)); // action
+  base = mrb_define_class_under(mrb, action, "Base", mrb->object_class);
+  MRB_SET_INSTANCE_TT(base, MRB_TT_DATA);
+  mrb_define_method(mrb, base, "initialize", mrb_action_base_initialize, MRB_ARGS_REQ(1)); // callback
+
+  move_to = mrb_define_class_under(mrb, action, "MoveTo", base);
+  MRB_SET_INSTANCE_TT(move_to, MRB_TT_DATA);
+  mrb_define_method(mrb, move_to, "initialize", mrb_action_move_to_initialize, MRB_ARGS_REQ(4)); // duration,x,y,callback
+
+  rotate_to = mrb_define_class_under(mrb, action, "RotateTo", base);
+  MRB_SET_INSTANCE_TT(rotate_to, MRB_TT_DATA);
+  mrb_define_method(mrb, rotate_to, "initialize", mrb_action_rotate_to_initialize, MRB_ARGS_REQ(3)); // duration,angle,callback
+
+  rotate_by = mrb_define_class_under(mrb, action, "RotateBy", base);
+  MRB_SET_INSTANCE_TT(rotate_by, MRB_TT_DATA);
+  mrb_define_method(mrb, rotate_by, "initialize", mrb_action_rotate_by_initialize, MRB_ARGS_REQ(3)); // duration,angle,callback
+
+  repeat = mrb_define_class_under(mrb, action, "Repeat", base);
+  MRB_SET_INSTANCE_TT(repeat, MRB_TT_DATA);
+  mrb_define_method(mrb, repeat, "initialize", mrb_action_repeat_initialize, MRB_ARGS_REQ(2)); // action, callback
+
+  sequence = mrb_define_class_under(mrb, action, "Sequence", base);
+  MRB_SET_INSTANCE_TT(sequence, MRB_TT_DATA);
+  mrb_define_method(mrb, sequence, "initialize", mrb_action_sequence_initialize, MRB_ARGS_REQ(2)); // actions, callback
 }
